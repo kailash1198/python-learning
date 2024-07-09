06ad77bf6ba6356dee1d72f956fd9e2fec453aa4
kailash kumar <kailash414243@gmail.com>
2024-07-09 21:48:17 +0530

    list in python

---
+++ 18-list.py
@@ -0,0 +1,17 @@
+# List - built in types in python but this is not same as Array(array come from external import library numPY)
+
+myList = ['item-1', 'item-2', 'item-3']
+print(myList)
+
+print(type(myList))
+
+print(len(myList))
+print(myList[0])
+print(myList[1])
+print(myList[2])
+
+listTwo = ['item-4']
+
+myList.append(listTwo)
+print(myList)
+print(myList[3][0])
