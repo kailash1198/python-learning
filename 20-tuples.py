9abfae32241259a4409b4e443d94b4e5ff9de44a
kailash kumar <kailash414243@gmail.com>
2024-07-11 22:10:47 +0530

    tuples

---
+++ 20-tuples.py
@@ -0,0 +1,12 @@
+# Tuples 
+
+thisTuple = ('Banana', 'Orange', 'Apple')
+print(thisTuple)
+
+#thisTuple[0] = 'Kailash' #Error- because tuple are un-changable
+
+myList = (1,2,3,4)
+if 1 in myList:
+    print('Yes')
+else:
+    print('Not')
