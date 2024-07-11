85c0d0e55861ff01f07586dca36723d5f873de34
kailash kumar <kailash414243@gmail.com>
2024-07-11 20:43:41 +0530

    sets practice

---
+++ 19-sets.py
@@ -0,0 +1,16 @@
+# Sets in python 
+# mySets = {1,2,3,4,5}
+# print(mySets)
+
+# print(type(mySets))
+
+
+setsTwo = {'kailash', 'kumar', 'aryan'}
+# print(setsTwo)
+# print(type(setsTwo))
+
+setsTwo.add('Laxmi')
+# print(setsTwo)
+
+setsTwo.remove('Laxmi')
+print(setsTwo)
