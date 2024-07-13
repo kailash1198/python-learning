2f9ec13531ceb3bf7a3d456b90ce9dcd99f36864
kailash kumar <kailash414243@gmail.com>
2024-07-13 23:57:31 +0530

    dictionary practice

---
+++ 21-dictionary.py
@@ -0,0 +1,15 @@
+# Dictionary types 
+
+myDictionaryList = {
+    "userName": 'Kailash',
+    "UserLocation": 'Bangalore',
+    "userClass" : 10
+}
+
+print(myDictionaryList)
+print(type(myDictionaryList))
+
+for i in myDictionaryList:
+    print(i)
+    
+print(len(myDictionaryList))
