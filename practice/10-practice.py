dfd64da08c97871e1488370a7547e1e2fe25ff03
kailash kumar <kailash414243@gmail.com>
2024-07-17 01:02:29 +0530

    python data types practice

---
+++ practice/10-practice.py
@@ -0,0 +1,35 @@
+# ➡️List type 
+# numberList = [1,2,3,4,5]
+# print(numberList)
+# print(type(numberList))
+
+# for i in numberList:
+#     print(i)
+
+
+# ➡️Tuples type 
+# tupleOne = ('Kailash', 'Shyam')
+# print(tupleOne)
+# print(type(tupleOne))
+
+# ➡️Sets type 
+# setOne = {"Kailash", "Kumar"}
+# print(setOne)
+# print(type(setOne))
+
+
+# ➡️Dictionary type 
+# student = {
+#     "firstName" : "Kailash",
+#     "lastName ": "Kumar",
+#     "Age": 26,
+#     "Semester": 3
+# }
+
+# print(student)
+    
+# print(student["firstName"])
+
+# if(student["firstName"]=='Kailash'):
+#     print("Your name is Kailash Kumar")
+    
