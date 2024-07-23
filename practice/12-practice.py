eef54ad045e8620211bd7f3e58fe9c7935e380ff
kailash kumar <kailash414243@gmail.com>
2024-07-24 01:05:25 +0530

    array practice

---
+++ practice/12-practice.py
@@ -0,0 +1,44 @@
+import array
+
+# numberList = array.array('i', [1,2,3,4,5])
+# print(numberList)
+
+# for i in numberList:
+#     print(i)
+    
+# searchKey = 3
+# for i in numberList:
+#     if(i==searchKey):
+#         print('Found : ' ,i)
+      
+#     else:
+#         print('Search key not found')
+#         continue
+
+# numberList.append(10)
+# numberList.append(11)
+# numberList.append(12)
+# numberList.append(13)
+# numberList.append(14)
+
+# for i in numberList:
+#     print(i)
+
+# coutTotal = numberList.count(0)
+
+
+# print(coutTotal)
+
+# numberList.pop(0)
+    
+# numberList.remove(5)
+# for i in numberList:
+#     print(i)
+    
+    
+    
+studentRoll = array.array('I', [100,101,102,103,104,105])
+studentRollTwo = (106,107,108,109,110,111)
+studentRoll.append(studentRollTwo)
+for i in studentRoll:
+    print(i)
