9292faae7e20da144bba158435ffbb06bf2e65b4
kailash kumar <kailash414243@gmail.com>
2024-08-07 22:42:18 +0530

    day 3 of 100 days python learning

---
+++ codewithharry/3day.py
@@ -0,0 +1,8 @@
+# Day 3 of learning python 100 days challange with codewithharry 
+# Modules and PIP(command)
+#pip - pip is windows package manager for install package from internet. Using pip command we can import external package from internet and we can use in our program.
+
+#external module - you need to download from internet
+import pandas
+#built in module - In built python module
+import hashlib
