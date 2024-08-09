0a884c70ffb8d7c7354064980b6c638d9c635c51
kailash kumar <kailash414243@gmail.com>
2024-08-09 22:58:07 +0530

    day 5 of learning python with code with harry 100 days challange

---
+++ codewithharry/5day.py
@@ -0,0 +1,33 @@
+#Day 5 of lrarning 100 days python with codewithharry
+
+#sinle line comment
+#Learn abou#Author = Kailash Kumar
+#Course - 100 days python code wtih harry
+
+#multi line comment
+'''
+line-1
+line-2
+line-3
+
+'''
+
+#Print statement program - Error because you went new line but you cannot do without skip sequence
+#\n is skip sequence
+# print("Hey, i am good boy \n and and thus viewer is also a good boy")
+
+#\" content which u want insert in quote \"
+# print('This is my coding \"kailash\" technology')
+
+
+# print(1,2,3,4,5)
+
+# print('hey','Kailash', sep="~")
+
+# print(1,2,3, end="009\n")
+# print('Hello')
+
+
+
+
+
