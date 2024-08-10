c30f27c03c34727e90bed58490fb89f5d0455422
kailash kumar <kailash414243@gmail.com>
2024-08-10 21:49:26 +0530

    day 6 of python learning with code with harry

---
+++ codewithharry/6day.py
@@ -0,0 +1,33 @@
+# Day 6 of 100 days python learning with code with harry 
+# variables
+# Data types
+#
+#
+
+a=1
+print(a)
+
+b= "kailash"
+print(b)
+
+d=None
+print(d)
+
+e=True
+print(e)
+
+print(type(d))
+
+print(type(a))
+print(type(e))
+
+complexNumber = complex(10,20)
+print(complexNumber)
+
+myDisctionary = {
+    "firstName":"Kailash",
+    "lastName": "Kumar"
+}
+
+print(myDisctionary)
+
