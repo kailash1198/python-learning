3f4ec6529bee9df3dbea97251a01914cc9600ce1
kailash kumar <kailash414243@gmail.com>
2024-08-12 22:18:20 +0530

    simple calculator exercise with day 7 of 100 day python learning challange

---
+++ codewithharry/7day.py
@@ -0,0 +1,19 @@
+# Day of learning 100 days python challange with code with harry 
+# calculator program exercise
+
+
+# print(5+10)
+
+print(5**3)
+
+print(15//6)
+
+one=10
+two=20
+
+print(one+two)
+print(two-one)
+print(one*two)
+print(one**two)
+print(one/two)
+print(one//two)
