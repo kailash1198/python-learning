d4d51ca51858bce2a090deba1b785630574ae33f
kailash kumar <kailash414243@gmail.com>
2024-08-14 23:47:47 +0530

    type casting

---
+++ codewithharry/9day.py
@@ -0,0 +1,14 @@
+# day9 of 100 days python learning challange with code with harry
+#type casting
+
+numberOne='20'
+print(numberOne)
+
+print(type(numberOne) )
+
+newNumber = int(numberOne)
+print(type(newNumber))
+
+
+numOne=10
+print(type(numOne))
