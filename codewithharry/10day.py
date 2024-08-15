a55c7b6cb470124acf253b0bb319858d7ef25aa2
kailash kumar <kailash414243@gmail.com>
2024-08-15 23:14:45 +0530

    input function understanding

---
+++ codewithharry/10day.py
@@ -0,0 +1,10 @@
+# Day 10 of 100 days of python learning with code with harry
+# Taking user input
+#input() function take input always as a string. When you type number then input function also store string format but not number type. In you need number type then you need to type casting or use int() function for changing string to number type.
+
+# userName = input('Enter your name')
+# print('User name is : ', userName)
+
+
+numInput = int(input('Enter number: '))
+print(type(numInput))
