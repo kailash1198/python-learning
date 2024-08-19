227edb36ecd1b5e86f025e3bfcc56ae360b7171d
kailash kumar <kailash414243@gmail.com>
2024-08-19 23:47:09 +0530

    string methods

---
+++ codewithharry/13day.py
@@ -0,0 +1,29 @@
+# String method
+#String are immutable that means you cannot change string. Instead string are create another variable to store new values.
+
+#string name
+myString = 'Kailash'
+
+print(len(myString))
+
+#method for string
+print(myString.upper())
+print(myString.lower())
+
+myName='Kailash!!!! Kumar'
+print(myName)
+print(myName.rstrip('!'))
+
+print(myName.split(" ")) #for create list
+
+blogHeading = 'introduction to JS'
+print(blogHeading.capitalize()) #for first letter uppercase in word other word become lower case auto
+
+findText='I am Kailash'
+print(findText.find('am'))
+
+text = 'I am aryan'
+print(text.index('ar'))
+
+
+
