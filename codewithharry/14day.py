550ed31418429256811633ec0dbb29597f30bd22
kailash kumar <kailash414243@gmail.com>
2024-08-20 23:56:37 +0530

    if else

---
+++ codewithharry/14day.py
@@ -0,0 +1,14 @@
+# Day 14 of 100days python learning challange with codewithharry
+#if else statement
+
+numOne=10
+numTwo=20
+
+if numOne<numTwo:
+    print('Numone is less then numtwo')
+    if(numOne==10):
+        print('Numone==10')
+elif numOne==numTwo:
+    print('numone==numtwo')
+else:
+    print('Erros') 
